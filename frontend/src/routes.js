66a55867e72404d38421615ea6e473f6d5ef7b1c
mikelxk <mike.lxk@gmail.com>
2022-03-22 10:12:57 -0400

    feat: overall and demo

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,15 +2,17 @@ import { Navigate, useRoutes } from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+import Blog from './pages/Blog';
+import DashboardApp from './pages/DashboardApp';
+import Demographic from './pages/Demographic';
+import Home from './pages/Home';
 //
 import Login from './pages/Login';
-import Register from './pages/Register';
-import DashboardApp from './pages/DashboardApp';
-import Products from './pages/Products';
-import Blog from './pages/Blog';
-import User from './pages/User';
+import Overall from './pages/Overall';
 import NotFound from './pages/Page404';
-import Home from './pages/Home';
+import Products from './pages/Products';
+import Register from './pages/Register';
+import User from './pages/User';
 // ----------------------------------------------------------------------
 
 export default function Router() {
@@ -30,6 +32,8 @@ export default function Router() {
       element: <LogoOnlyLayout />,
       children: [
         { path: '/', element: <Home /> },
+        { path: 'demographic', element: <Demographic /> },
+        { path: 'overall', element: <Overall /> },
         { path: 'login', element: <Login /> },
         { path: 'register', element: <Register /> },
         { path: '404', element: <NotFound /> },
