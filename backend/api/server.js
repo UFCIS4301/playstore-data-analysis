f4c01f1c1a142fdc44571cdab0fb647fb5a5c0d5
Rajdev Kapoor <rajdev1409@gmail.com>
2022-04-09 03:12:35 -0400

    added left apis

--- backend/api/server.js
+++ backend/api/server.js
@@ -83,7 +83,31 @@ app.get('/api/demographic', async function(req, res) {
 	}
 });
 
-app.get('/api/hello', async function(req, res) {
+app.get('/api/avgappsize', async function(req, res) {
+	//res.send({ express: 'Hello From Express' });
+	let conn;
+	try {
+		conn = await oracledb.getConnection(config);
+
+		const result = await conn.execute(
+			"SELECT ROUND(AVG(Appsize)), Released_Year, Domain.Category FROM \"SINHA.KSHITIJ\".Appdetails, \"SINHA.KSHITIJ\".Dates, \"SINHA.KSHITIJ\".Applications, \"SINHA.KSHITIJ\".Domain Where Applications.AppID = Appdetails.ID and Applications.AppID = Dates.ID and Applications.DomainID = Domain.Domain_ID and Category IN ('Action', 'Entertainment', 'Social', 'Shopping', 'Productivity') Group BY Released_Year, Domain.Category Order by Released_YEar desc",
+			//'select * from "SINHA.KSHITIJ".applications',
+			{}
+		);
+
+		res.send(JSON.stringify(result));
+		//console.log(result);
+	} catch (err) {
+		console.log('Ouch!', err);
+	} finally {
+		if (conn) {
+			// conn assignment worked, need to close
+			await conn.close();
+		}
+	}
+});
+
+app.get('/api/totalappcount', async function(req, res) {
 	//res.send({ express: 'Hello From Express' });
 	let conn;
 	try {
@@ -92,10 +116,76 @@ app.get('/api/hello', async function(req, res) {
 		const result = await conn.execute(
 			'SELECT COUNT(Appdetails.ID), Released_YEar FROM "SINHA.KSHITIJ".Appdetails, "SINHA.KSHITIJ".Dates WHERE Appdetails.ID = Dates.ID Group by Released_YEar Order by Released_Year desc',
 			//'select * from "SINHA.KSHITIJ".applications',
-			{},
-			{
-				outFormat: oracledb.OBJECT
-			}
+			{}
+		);
+
+		res.send(JSON.stringify(result));
+		//console.log(result);
+	} catch (err) {
+		console.log('Ouch!', err);
+	} finally {
+		if (conn) {
+			// conn assignment worked, need to close
+			await conn.close();
+		}
+	}
+});
+
+app.get('/api/avgappsizevstotalcount', async function(req, res) {
+	//res.send({ express: 'Hello From Express' });
+	let conn;
+	try {
+		conn = await oracledb.getConnection(config);
+
+		const result = await conn.execute(
+			'SELECT COUNT(AppID), ROUND(AVG(Appsize)), Released_Year FROM "SINHA.KSHITIJ".Appdetails, "SINHA.KSHITIJ".Dates, "SINHA.KSHITIJ".Applications, "SINHA.KSHITIJ".Domain	Where Applications.AppID = Appdetails.ID and Applications.AppID = Dates.ID and Applications.DomainID = Domain.Domain_ID Group BY Released_Year	Order by Released_YEar desc',
+			{}
+		);
+
+		res.send(JSON.stringify(result));
+		//console.log(result);
+	} catch (err) {
+		console.log('Ouch!', err);
+	} finally {
+		if (conn) {
+			// conn assignment worked, need to close
+			await conn.close();
+		}
+	}
+});
+
+app.get('/api/avgprice', async function(req, res) {
+	//res.send({ express: 'Hello From Express' });
+	let conn;
+	try {
+		conn = await oracledb.getConnection(config);
+
+		const result = await conn.execute(
+			'SELECT COUNT(AppID), ROUND(AVG(Price)), Released_Year FROM "SINHA.KSHITIJ".Appdetails, "SINHA.KSHITIJ".Dates, "SINHA.KSHITIJ".Applications Where Applications.AppID = Appdetails.ID and Applications.AppID = Dates.ID and Free=\'No\' Group BY Released_Year Order by Released_YEar desc',
+			{}
+		);
+
+		res.send(JSON.stringify(result));
+		//console.log(result);
+	} catch (err) {
+		console.log('Ouch!', err);
+	} finally {
+		if (conn) {
+			// conn assignment worked, need to close
+			await conn.close();
+		}
+	}
+});
+
+app.get('/api/paidratio', async function(req, res) {
+	//res.send({ express: 'Hello From Express' });
+	let conn;
+	try {
+		conn = await oracledb.getConnection(config);
+
+		const result = await conn.execute(
+			'SELECT ROUND((t1.Paid/t2.Free)*100) as Ratio, Released_Year FROM 			(SELECT Count(AppId) as Paid, Released_YEar			FROM "SINHA.KSHITIJ".Appdetails, "SINHA.KSHITIJ".Dates, "SINHA.KSHITIJ".Applications			Where Applications.AppID = Appdetails.ID and Applications.AppID = Dates.ID and Free=\'No\'			Group BY Released_Year) t1			NATURAL JOIN			(SELECT Count(AppId) as Free, Released_YEar			FROM "SINHA.KSHITIJ".Appdetails, "SINHA.KSHITIJ".Dates, "SINHA.KSHITIJ".Applications			Where Applications.AppID = Appdetails.ID and Applications.AppID = Dates.ID and Free=\'Yes\'			Group BY Released_Year) t2			ORDER By Released_Year DEsc',
+			{}
 		);
 
 		res.send(JSON.stringify(result));
