e4b3f5563b876b76c92ed9edf8d02b826517738b
Kshitij Sinha <43610780+sinarest1608@users.noreply.github.com>
2022-04-10 20:04:44 -0400

    Added Chart: Paid Vs Free

---
+++ frontend/src/demo-data/paid-vs-free.js
@@ -0,0 +1,83 @@
+export const paidVsFreeData = // 20220410194110
+// http://localhost:5000/api/paidvsfree
+
+{
+  "metaData": [
+    {
+      "name": "PAID"
+    },
+    {
+      "name": "FREE"
+    },
+    {
+      "name": "RELEASED_YEAR"
+    }
+  ],
+  "rows": [
+    {
+      "PAID": 7617,
+      "FREE": 656777,
+      "RELEASED_YEAR": 2021
+    },
+    {
+      "PAID": 10460,
+      "FREE": 716560,
+      "RELEASED_YEAR": 2020
+    },
+    {
+      "PAID": 8166,
+      "FREE": 423881,
+      "RELEASED_YEAR": 2019
+    },
+    {
+      "PAID": 5299,
+      "FREE": 217276,
+      "RELEASED_YEAR": 2018
+    },
+    {
+      "PAID": 3961,
+      "FREE": 130914,
+      "RELEASED_YEAR": 2017
+    },
+    {
+      "PAID": 3013,
+      "FREE": 63301,
+      "RELEASED_YEAR": 2016
+    },
+    {
+      "PAID": 2063,
+      "FREE": 36236,
+      "RELEASED_YEAR": 2015
+    },
+    {
+      "PAID": 2180,
+      "FREE": 14772,
+      "RELEASED_YEAR": 2014
+    },
+    {
+      "PAID": 1555,
+      "FREE": 5453,
+      "RELEASED_YEAR": 2013
+    },
+    {
+      "PAID": 465,
+      "FREE": 1862,
+      "RELEASED_YEAR": 2012
+    },
+    {
+      "PAID": 237,
+      "FREE": 661,
+      "RELEASED_YEAR": 2011
+    },
+    {
+      "PAID": 51,
+      "FREE": 171,
+      "RELEASED_YEAR": 2010
+    },
+    {
+      "PAID": 1,
+      "FREE": 12,
+      "RELEASED_YEAR": 2009
+    }
+  ]
+}
