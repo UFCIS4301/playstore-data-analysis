a72957d4c5db2a017f999b342906d7d6a9559dc7
mikelxk <mike.lxk@gmail.com>
2022-04-13 18:07:10 -0400

    feat: add total count

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,13 +1,13 @@
 import { EventTracker, Stack, ValueScale } from '@devexpress/dx-react-chart';
 import {
-	ArgumentAxis,
-	BarSeries,
-	Chart,
-	Legend,
-	LineSeries,
-	Title,
-	Tooltip,
-	ValueAxis
+  ArgumentAxis,
+  BarSeries,
+  Chart,
+  Legend,
+  LineSeries,
+  Title,
+  Tooltip,
+  ValueAxis
 } from '@devexpress/dx-react-chart-material-ui';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -16,7 +16,9 @@ import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import useSWR from 'swr';
 
-const Root = (props) => <Legend.Root {...props} sx={{ display: 'flex', margin: 'auto', flexDirection: 'row' }} />;
+const Root = (props) => (
+  <Legend.Root {...props} sx={{ display: 'flex', margin: 'auto', flexDirection: 'row' }} />
+);
 const LabelA = (props) => <Legend.Label {...props} sx={{ whiteSpace: 'nowrap' }} />;
 const Item = (props) => <Legend.Item {...props} sx={{ flexDirection: 'column-reverse' }} />;
 
@@ -34,7 +36,21 @@ const m2019 = new Map();
 const m2020 = new Map();
 const m2021 = new Map();
 
-const list = [ m2009, m2010, m2011, m2012, m2013, m2014, m2015, m2016, m2017, m2018, m2019, m2020, m2021 ];
+const list = [
+  m2009,
+  m2010,
+  m2011,
+  m2012,
+  m2013,
+  m2014,
+  m2015,
+  m2016,
+  m2017,
+  m2018,
+  m2019,
+  m2020,
+  m2021
+];
 // Education
 // Entertainment
 // Casual
@@ -49,265 +65,270 @@ const list = [ m2009, m2010, m2011, m2012, m2013, m2014, m2015, m2016, m2017, m2
 // Personalization
 
 const Label = (symbol) => (props) => {
-	const { text } = props;
-	return <ValueAxis.Label {...props} text={text + symbol} />;
+  const { text } = props;
+  return <ValueAxis.Label {...props} text={text + symbol} />;
 };
 
-const modifyCatCountDomain = (domain) => [ domain[0], 75000 ];
-const modifyTotalDomain = () => [ 0, 750000 ];
+const modifyCatCountDomain = (domain) => [domain[0], 75000];
+const modifyTotalDomain = () => [0, 750000];
 export default function Category() {
-	const [ categoryData, setCateData ] = React.useState([]);
-	const { data } = useSWR('/api/category');
-	if (data && categoryData.length === 0) {
-		list.forEach((e) => {
-			e.set('Education', 0);
-			e.set('Entertainment', 0);
-			e.set('Casual', 0);
-			e.set('Business', 0);
-			e.set('Productivity', 0);
-			e.set('Music and Audio', 0);
-			e.set('Tools', 0);
-			e.set('Arcade', 0);
-			e.set('Lifestyle', 0);
-			e.set('Puzzle', 0);
-			e.set('Books and Reference', 0);
-			e.set('Personalization', 0);
-		});
+  const [categoryData, setCateData] = React.useState([]);
+  const { data } = useSWR('/api/category');
+  if (data && categoryData.length === 0) {
+    list.forEach((e) => {
+      e.set('Education', 0);
+      e.set('Entertainment', 0);
+      e.set('Casual', 0);
+      e.set('Business', 0);
+      e.set('Productivity', 0);
+      e.set('Music and Audio', 0);
+      e.set('Tools', 0);
+      e.set('Arcade', 0);
+      e.set('Lifestyle', 0);
+      e.set('Puzzle', 0);
+      e.set('Books and Reference', 0);
+      e.set('Personalization', 0);
+    });
 
-		data.rows.forEach((row) => {
-			if (row.RELEASED_YEAR === 2021) {
-				m2021.set('Category', row.CATEGORY);
-				m2021.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2021.set('Total', row.TOTAL_APPLICATION);
-				m2021.set('Rank', row.RANK);
-				m2021.set('year', '2021');
-			} else if (row.RELEASED_YEAR === 2020) {
-				m2020.set('Category', row.CATEGORY);
-				m2020.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2020.set('Total', row.TOTAL_APPLICATION);
-				m2020.set('year', '2020');
-			} else if (row.RELEASED_YEAR === 2019) {
-				m2019.set('Category', row.CATEGORY);
-				m2019.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2019.set('Total', row.TOTAL_APPLICATION);
-				m2019.set('year', '2019');
-			} else if (row.RELEASED_YEAR === 2018) {
-				m2018.set('Category', row.CATEGORY);
-				m2018.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2018.set('Total', row.TOTAL_APPLICATION);
-				m2018.set('year', '2018');
-			} else if (row.RELEASED_YEAR === 2017) {
-				m2017.set('Category', row.CATEGORY);
-				m2017.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2017.set('Total', row.TOTAL_APPLICATION);
-				m2017.set('year', '2017');
-			} else if (row.RELEASED_YEAR === 2016) {
-				m2016.set('Category', row.CATEGORY);
-				m2016.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2016.set('Total', row.TOTAL_APPLICATION);
-				m2016.set('year', '2016');
-			} else if (row.RELEASED_YEAR === 2015) {
-				m2015.set('Category', row.CATEGORY);
-				m2015.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2015.set('Total', row.TOTAL_APPLICATION);
-				m2015.set('year', '2015');
-			} else if (row.RELEASED_YEAR === 2014) {
-				m2014.set('Category', row.CATEGORY);
-				m2014.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2014.set('Total', row.TOTAL_APPLICATION);
-				m2014.set('year', '2014');
-			} else if (row.RELEASED_YEAR === 2013) {
-				m2013.set('Category', row.CATEGORY);
-				m2013.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2013.set('Total', row.TOTAL_APPLICATION);
-				m2013.set('year', '2013');
-			} else if (row.RELEASED_YEAR === 2012) {
-				m2012.set('Category', row.CATEGORY);
-				m2012.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2012.set('Total', row.TOTAL_APPLICATION);
-				m2012.set('year', '2012');
-			} else if (row.RELEASED_YEAR === 2011) {
-				m2011.set('Category', row.CATEGORY);
-				m2011.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2011.set('Total', row.TOTAL_APPLICATION);
-				m2011.set('year', '2011');
-			} else if (row.RELEASED_YEAR === 2010) {
-				m2010.set('Category', row.CATEGORY);
-				m2010.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2010.set('Total', row.TOTAL_APPLICATION);
-				m2010.set('year', '2010');
-			} else if (row.RELEASED_YEAR === 2009) {
-				m2009.set('Category', row.CATEGORY);
-				m2009.set(row.CATEGORY, row.CATEGORY_COUNT);
-				m2009.set('Total', row.TOTAL_APPLICATION);
-				m2009.set('year', '2009');
-			}
-		});
+    data.rows.forEach((row) => {
+      if (row.RELEASED_YEAR === 2021) {
+        m2021.set('Category', row.CATEGORY);
+        m2021.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2021.set('Total', row.TOTAL_APPLICATION);
+        m2021.set('Rank', row.RANK);
+        m2021.set('year', '2021');
+      } else if (row.RELEASED_YEAR === 2020) {
+        m2020.set('Category', row.CATEGORY);
+        m2020.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2020.set('Total', row.TOTAL_APPLICATION);
+        m2020.set('year', '2020');
+      } else if (row.RELEASED_YEAR === 2019) {
+        m2019.set('Category', row.CATEGORY);
+        m2019.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2019.set('Total', row.TOTAL_APPLICATION);
+        m2019.set('year', '2019');
+      } else if (row.RELEASED_YEAR === 2018) {
+        m2018.set('Category', row.CATEGORY);
+        m2018.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2018.set('Total', row.TOTAL_APPLICATION);
+        m2018.set('year', '2018');
+      } else if (row.RELEASED_YEAR === 2017) {
+        m2017.set('Category', row.CATEGORY);
+        m2017.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2017.set('Total', row.TOTAL_APPLICATION);
+        m2017.set('year', '2017');
+      } else if (row.RELEASED_YEAR === 2016) {
+        m2016.set('Category', row.CATEGORY);
+        m2016.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2016.set('Total', row.TOTAL_APPLICATION);
+        m2016.set('year', '2016');
+      } else if (row.RELEASED_YEAR === 2015) {
+        m2015.set('Category', row.CATEGORY);
+        m2015.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2015.set('Total', row.TOTAL_APPLICATION);
+        m2015.set('year', '2015');
+      } else if (row.RELEASED_YEAR === 2014) {
+        m2014.set('Category', row.CATEGORY);
+        m2014.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2014.set('Total', row.TOTAL_APPLICATION);
+        m2014.set('year', '2014');
+      } else if (row.RELEASED_YEAR === 2013) {
+        m2013.set('Category', row.CATEGORY);
+        m2013.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2013.set('Total', row.TOTAL_APPLICATION);
+        m2013.set('year', '2013');
+      } else if (row.RELEASED_YEAR === 2012) {
+        m2012.set('Category', row.CATEGORY);
+        m2012.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2012.set('Total', row.TOTAL_APPLICATION);
+        m2012.set('year', '2012');
+      } else if (row.RELEASED_YEAR === 2011) {
+        m2011.set('Category', row.CATEGORY);
+        m2011.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2011.set('Total', row.TOTAL_APPLICATION);
+        m2011.set('year', '2011');
+      } else if (row.RELEASED_YEAR === 2010) {
+        m2010.set('Category', row.CATEGORY);
+        m2010.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2010.set('Total', row.TOTAL_APPLICATION);
+        m2010.set('year', '2010');
+      } else if (row.RELEASED_YEAR === 2009) {
+        m2009.set('Category', row.CATEGORY);
+        m2009.set(row.CATEGORY, row.CATEGORY_COUNT);
+        m2009.set('Total', row.TOTAL_APPLICATION);
+        m2009.set('year', '2009');
+      }
+    });
 
-		const combinedVersdata = [
-			//   Object.fromEntries(m2009),
-			// Object.fromEntries(m2010),
-			Object.fromEntries(m2011),
-			Object.fromEntries(m2012),
-			Object.fromEntries(m2013),
-			Object.fromEntries(m2014),
-			Object.fromEntries(m2015),
-			Object.fromEntries(m2016),
-			Object.fromEntries(m2017),
-			Object.fromEntries(m2018),
-			Object.fromEntries(m2019),
-			Object.fromEntries(m2020),
-			Object.fromEntries(m2021)
-		];
-		setCateData(combinedVersdata);
-	}
-	return (
-		<div className="parentdiv">
-			<div className="dyk">
-				<Card sx={{ maxWidth: '100%', border: 0, boxShadow: 4 }}>
-					<CardContent>
-						<Typography gutterBottom variant="h4" component="div">
-							Do You Know?
-						</Typography>
-						<Typography variant="h6" color="text.secondary">
-							Globally, there are more than 1 billion Google play users.
-						</Typography>
-					</CardContent>
-				</Card>
-			</div>
-			<Card sx={{ maxWidth: '100%', border: 0, boxShadow: 10 }}>
-				<Paper>
-					<Chart data={categoryData}>
-						<ValueScale name="Total" modifyDomain={modifyTotalDomain} />
-						<ValueScale name="Category Count" modifyDomain={modifyCatCountDomain} />
+    const combinedVersdata = [
+      //   Object.fromEntries(m2009),
+      // Object.fromEntries(m2010),
+      Object.fromEntries(m2011),
+      Object.fromEntries(m2012),
+      Object.fromEntries(m2013),
+      Object.fromEntries(m2014),
+      Object.fromEntries(m2015),
+      Object.fromEntries(m2016),
+      Object.fromEntries(m2017),
+      Object.fromEntries(m2018),
+      Object.fromEntries(m2019),
+      Object.fromEntries(m2020),
+      Object.fromEntries(m2021)
+    ];
+    setCateData(combinedVersdata);
+  }
+  return (
+    <div className="parentdiv">
+      <div className="dyk">
+        <Card sx={{ maxWidth: '100%', border: 0, boxShadow: 4 }}>
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              Do You Know?
+            </Typography>
+            <Typography variant="h6" color="text.secondary">
+              Globally, there are more than 1 billion Google play users.
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+      <Card sx={{ maxWidth: '100%', border: 0, boxShadow: 10 }}>
+        <Paper>
+          <Chart data={categoryData}>
+            <ValueScale name="Total" modifyDomain={modifyTotalDomain} />
+            <ValueScale name="Category Count" modifyDomain={modifyCatCountDomain} />
 
-						<ArgumentAxis />
-						<ValueAxis scaleName="Category Count" />
-						<ValueAxis scaleName="Total" position="right" />
+            <ArgumentAxis />
+            <ValueAxis scaleName="Category Count" />
+            <ValueAxis scaleName="Total" position="right" />
 
-						<BarSeries
-							name="Education"
-							valueField="Education"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Entertainment"
-							valueField="Entertainment"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Casual"
-							valueField="Casual"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Business"
-							valueField="Business"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
+            <BarSeries
+              name="Education"
+              valueField="Education"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Entertainment"
+              valueField="Entertainment"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Casual"
+              valueField="Casual"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Business"
+              valueField="Business"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
 
-						<BarSeries
-							name="Productivity"
-							valueField="Productivity"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Music and Audio"
-							valueField="Music and Audio"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Tools"
-							valueField="Tools"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
+            <BarSeries
+              name="Productivity"
+              valueField="Productivity"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Music and Audio"
+              valueField="Music and Audio"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Tools"
+              valueField="Tools"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
 
-						<BarSeries
-							name="Arcade"
-							valueField="Arcade"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Lifestyle"
-							valueField="Lifestyle"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Puzzle"
-							valueField="Puzzle"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Books and Reference"
-							valueField="Books and Reference"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<BarSeries
-							name="Personalization"
-							valueField="Personalization"
-							argumentField="year"
-							// color='orange'
-							scaleName="Category Count"
-						/>
-						<LineSeries
-							name="Total"
-							valueField="Total"
-							argumentField="year"
-							scaleName="Total"
-							color="violet"
-						/>
+            <BarSeries
+              name="Arcade"
+              valueField="Arcade"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Lifestyle"
+              valueField="Lifestyle"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Puzzle"
+              valueField="Puzzle"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Books and Reference"
+              valueField="Books and Reference"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <BarSeries
+              name="Personalization"
+              valueField="Personalization"
+              argumentField="year"
+              // color='orange'
+              scaleName="Category Count"
+            />
+            <LineSeries
+              name="Total"
+              valueField="Total"
+              argumentField="year"
+              scaleName="Total"
+              color="violet"
+            />
 
-						{/* <Animation /> */}
-						{/* <Stack
+            {/* <Animation /> */}
+            {/* <Stack
             stacks={[
               { series: ['Paid', 'Price'] },
             ]}
           /> */}
-						<Legend position="bottom" rootComponent={Root} labelComponent={LabelA} itemComponent={Item} />
-						<Title text="Paid Application Launches and Avg Price over the years" />
-						<Stack />
-						<EventTracker />
-						<Tooltip />
-					</Chart>
-				</Paper>
-			</Card>
-			<div className="trendanalysis">
-				<Card sx={{ maxWidth: '100%', border: 0, boxShadow: 10 }}>
-					<CardContent>
-						<Typography gutterBottom variant="h4" component="div">
-							Trend Analysis
-						</Typography>
-						<Typography variant="h6" color="text.secondary">
-							This query allow us to present the number of app launches that occurred in a year or across
-							multiple years. This would provide insight into the app launch pattern over time. For
-							example, we may see which category had the most apps released on the Play Store in 2015
-							against how the same category is performing currently.
-						</Typography>
-					</CardContent>
-				</Card>
-			</div>
-		</div>
-	);
+            <Legend
+              position="bottom"
+              rootComponent={Root}
+              labelComponent={LabelA}
+              itemComponent={Item}
+            />
+            <Title text="Paid Application Launches and Avg Price over the years" />
+            <Stack />
+            <EventTracker />
+            <Tooltip />
+          </Chart>
+        </Paper>
+      </Card>
+      <div className="trendanalysis">
+        <Card sx={{ maxWidth: '100%', border: 0, boxShadow: 10 }}>
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="div">
+              Trend Analysis
+            </Typography>
+            <Typography variant="h6" color="text.secondary">
+              This query allow us to present the number of app launches that occurred in a year or
+              across multiple years. This would provide insight into the app launch pattern over
+              time. For example, we may see which category had the most apps released on the Play
+              Store in 2015 against how the same category is performing currently.
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,79 +1,83 @@
-import * as React from 'react';
-import useSWR from 'swr';
-import { Translate } from '@mui/icons-material';
 import { Box, Button, Container, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { motion } from 'framer-motion';
+import * as React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import useSWR from 'swr';
 // components
 import { MotionContainer, varBounceIn } from '../components/animate';
 import Page from '../components/Page';
 
 // ----------------------------------------------------------------------
 const queries = [
-	{ title: 'Query 1', url: '/paid-vs-free' },
-	{ title: 'Query 2', url: '/category' },
-	{ title: 'Query 3', url: '/demographic' },
-	{ title: 'Query 4', url: '/appversion' },
-	{ title: 'Query 5', url: '/size-vs-app' }
+  { title: 'Query 1', url: '/paid-vs-free' },
+  { title: 'Query 2', url: '/category' },
+  { title: 'Query 3', url: '/demographic' },
+  { title: 'Query 4', url: '/appversion' },
+  { title: 'Query 5', url: '/size-vs-app' }
 ];
 const RootStyle = styled(Page)(({ theme }) => ({
-	display: 'flex',
-	minHeight: '100%',
-	alignItems: 'center',
-	paddingTop: theme.spacing(15),
-	paddingBottom: theme.spacing(10)
+  display: 'flex',
+  minHeight: '100%',
+  alignItems: 'center',
+  paddingTop: theme.spacing(15),
+  paddingBottom: theme.spacing(10)
 }));
 
 // ----------------------------------------------------------------------
 
 export default function Home() {
-	const [ demoData, setDemoData ] = React.useState([]);
-	const { data } = useSWR('/api/totalappcount');
-	return (
-		<RootStyle title="Home">
-			<Container align="center">
-				<MotionContainer initial="initial" open>
-					<Box sx={{ margin: 'auto', textAlign: 'center' }}>
-						<motion.div variants={varBounceIn}>
-							<Typography variant="h1" paragraph>
-								Google PlayStore Trend Analysis
-							</Typography>
-						</motion.div>
+  const [demoData, setDemoData] = React.useState(0);
+  const { data } = useSWR('/api/totalappcount');
+  if (data && demoData === 0) {
+    setDemoData(data.rows[0].TOTAL_TUPLES);
+  }
+  return (
+    <RootStyle title="Home">
+      <Container align="center">
+        <MotionContainer initial="initial" open>
+          <Box sx={{ margin: 'auto', textAlign: 'center' }}>
+            <motion.div variants={varBounceIn}>
+              <Typography variant="h1" paragraph>
+                Google PlayStore Trend Analysis
+              </Typography>
+            </motion.div>
 
-						<Typography sx={{ color: 'text.secondary' }}>
-							Query below on data for Google Play Store
-						</Typography>
-						<div
-							className="queries"
-							style={{
-								gap: '15px',
-								display: 'flex',
-								marginTop: '60px',
-								transform: 'translate(-50%,-50%)',
-								marginLeft: '43%'
-							}}
-						>
-							{queries.map((query, index) => (
-								<Button
-									to={query.url}
-									size="large"
-									variant="contained"
-									component={RouterLink}
-									key={index}
-									style={{ minWidth: '137px' }}
-								>
-									{query.title}
-								</Button>
-							))}
-						</div>
-					</Box>
-					<br />
+            <Typography sx={{ color: 'text.secondary' }}>
+              Query below on data for Google Play Store
+            </Typography>
+            <div
+              className="queries"
+              style={{
+                gap: '15px',
+                display: 'flex',
+                marginTop: '60px',
+                transform: 'translate(-50%,-50%)',
+                marginLeft: '43%'
+              }}
+            >
+              {queries.map((query, index) => (
+                <Button
+                  to={query.url}
+                  size="large"
+                  variant="contained"
+                  component={RouterLink}
+                  key={index}
+                  style={{ minWidth: '137px' }}
+                >
+                  {query.title}
+                </Button>
+              ))}
+            </div>
+          </Box>
+          <br />
 
-					{/* api => api/totalappcount */}
-					<Button>Click me to reveal Number of tuples:2312944</Button>
-				</MotionContainer>
-			</Container>
-		</RootStyle>
-	);
+          {/* api => api/totalappcount */}
+          <Typography sx={{ color: 'text.secondary' }}>
+            There are {demoData} tuples in the database
+          </Typography>
+        </MotionContainer>
+      </Container>
+    </RootStyle>
+  );
 }
